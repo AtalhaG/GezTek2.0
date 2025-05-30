6bebfbd880c4376c93fb43ff3b347a74773a3dd6
Burak <buraksoysal08@gmail.com>
2025-05-30 12:05:23 +0300

    hatalar çözüldü

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,27 +6,23 @@ import FlutterMacOS
 import Foundation
 
 import cloud_firestore
+import file_picker
 import file_selector_macos
 import firebase_auth
 import firebase_core
-<<<<<<<<< Temporary merge branch 1
+import firebase_database
 import firebase_storage
 import path_provider_foundation
 import sqflite_darwin
-=========
-import firebase_database
->>>>>>>>> Temporary merge branch 2
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-<<<<<<<<< Temporary merge branch 1
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-=========
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
->>>>>>>>> Temporary merge branch 2
 }
